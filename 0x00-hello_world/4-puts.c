e71ac12223482c49aee88345186d12bd9896767c
Ahmed Abouzid <hurghada.police@gmail.com>
2023-06-19 00:50:23 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main()
 {
-	char str[50] = "\"Programming is like building a multilingual puzzle";
+	char str[50] = "Programming is like building a multilingual puzzle";
+	
 	puts(str);
+
 	return(0);
 }
 
---
+++ 0x00-hello_world/4-putstest.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	char str[5] = "Pro";
+	puts(str);
+	return(0);
+}
+
