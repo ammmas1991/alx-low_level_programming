0d44c9fbe5f8b347d0eaa3492841c290b924f515
Ahmed Abouzid <hurghada.police@gmail.com>
2023-06-21 03:11:08 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,15 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: 'the program print a string using the printf function'
- *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
-
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
 
