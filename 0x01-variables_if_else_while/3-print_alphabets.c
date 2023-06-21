a875480f196984cfc4a0898314c41c52045b0fcb
Ahmed Abouzid <hurghada.police@gmail.com>
2023-06-21 16:42:56 -0700

    m

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	char a = 'a';
+	char z = 'z';
+	char A = 'A';
+	char Z = 'Z';
+
+
+	while (a <= z)
+	{
+		
+		putchar(a);
+		a++;
+	}
+
+	while (A <= Z)
+	{
+		putchar(A);
+		A++;
+	}
+	putchar('\n');
+
+	return (0);
+}
