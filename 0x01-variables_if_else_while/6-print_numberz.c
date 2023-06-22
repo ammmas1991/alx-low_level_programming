6f02412fa9c31a291d18858deb83b304e870d431
Ahmed Abouzid <hurghada.police@gmail.com>
2023-06-22 02:39:09 -0700

    m

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	char a;
+
+        for (a = 0; a < 10; a++)
+	{
+		putchar(48+a);
+	}
+	putchar('\n');
+
+	return (0);
+}
